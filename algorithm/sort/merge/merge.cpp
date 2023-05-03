e1e97f7401c5ec340ae4f0d8a7f2d8de10e65890
ldbl1258 <ldbl1258@gmail.com>
2023-05-03 09:05:40 +0900

    add merge sort

--- algorithm/sort/merge/merge.cpp
+++ algorithm/sort/merge/merge.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm> // for random suffle 
+#include <cstring>
 
 
 int * gen_array(int _size)
@@ -17,12 +18,71 @@ void show_array(int * _array, int _size)
     std::cout << _array[i] << (i < _size-1 ? ",\t": "\n");
 }
 
+int * merge_sort(int * _array, int _size)
+{
+  int * array = new int[_size];
+  std::memcpy(array,_array, sizeof(int) * _size);
+
+
+   if(_size <= 1 )
+    return array;
+
+  int left_size = _size /2;
+  int right_size = _size - _size/2;
+
+  int * left_sort    = merge_sort(array, left_size);
+  int * right_sort   = merge_sort(array+left_size, right_size);
+  {
+    int lidx = 0;
+    int ridx = 0;
+    
+    while(lidx + ridx < _size)
+    {
+      if(lidx < left_size && ridx < right_size)
+      {
+        int min = (left_sort[lidx] < right_sort[ridx] ?
+           left_sort[lidx++] : 
+          right_sort[ridx++]);
+        array[lidx + ridx -1 ] = min;
+        continue;
+      }
+      if(!(lidx < left_size))
+      {
+        array[lidx + ridx] = right_sort[ridx];
+        ridx++;
+        continue;
+      }
+        
+
+
+      if(!(ridx < right_size))
+      {
+        array[lidx + ridx] = left_sort[lidx];
+        lidx++;
+        continue;
+      }
+
+    }
+    
+  }
+  std::cout << "------------------- sort -------------" << std::endl;
+
+show_array(left_sort, left_size);
+show_array(right_sort, right_size);
+    show_array(array,_size);
+  return array;
+
+}
+
 int main(int argc , char ** argv)
 {
 
   srand(argc > 1 ? std::stoi(argv[1]) : 0 );
   int * array = gen_array(10);
   show_array(array,10 );
+  std::cout << "sorted " << std::endl;
+  int * sort = merge_sort(array,10 );
+  show_array(sort , 10);
 
   return 0;
 }
--- algorithm/sort/selection/build/cmake_install.cmake
+++ algorithm/sort/selection/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/wolp/Akane_Chan/algorithm/sort/selection
+# Install script for directory: /home/wolp/Documents/Akane_Chan/algorithm/sort/selection
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -29,7 +29,7 @@ endif()
 
 # Install shared libraries without execute permission?
 if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "0")
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
 endif()
 
 # Is this installation the result of a crosscompile?
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/wolp/Akane_Chan/algorithm/sort/selection/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/wolp/Documents/Akane_Chan/algorithm/sort/selection/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
