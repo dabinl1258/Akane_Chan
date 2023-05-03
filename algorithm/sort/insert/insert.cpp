cca4320247b107fa2380278ae30a749520aece34
ldbl1258 <ldbl1258@gmail.com>
2023-05-04 01:15:35 +0900

    change .gitignore for CMake support

---
+++ algorithm/sort/insert/insert.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <bits/move.h>
+#include <algorithm> // for random suffle 
+
+
+int * gen_array(int _size)
+{
+  int * ret = new int[_size];
+  for(int i  = 1 ; i <= _size; i++)
+    ret[i -1] = i;
+  std::random_shuffle(ret, ret + _size);
+  return ret;
+}
+
+void show_array(int * _array, int _size)
+{
+  for(int i = 0; i < _size; i++)
+    std::cout << _array[i] << (i < _size-1 ? ",\t": "\n");
+}
+
+void selection_sort(int * _array, int _size)
+{
+  for(int un_sort =_size; un_sort > 1 ; un_sort--)
+  {
+    int idx_max = 0;
+    for(int idx = 1; idx < un_sort; idx++)
+    {
+        idx_max = (_array[idx_max] < _array[idx] ? idx : idx_max );
+    }
+    std::swap(_array[idx_max], _array[un_sort-1]);
+  }
+     
+}
+
+int main(int argc , char ** argv)
+{
+
+  srand(argc > 1 ? std::stoi(argv[1]) : 0 );
+  int * array = gen_array(10);
+  show_array(array,10 );
+  std::cout << "sort " << std::endl;
+
+  selection_sort(array, 10);
+  show_array(array,10);
+
+  return 0;
+}
